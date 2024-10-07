6f11f95f8562703984ccb96b0b2454b0b1016307
Guardianprime <guardianacer@outlook.com>
2024-10-07 16:15:20 +0100

    fixed the delete button

--- src/App.js
+++ src/App.js
@@ -15,39 +15,39 @@ export default function App() {
     </div>
     <div className="buttons-container">
       <ClearButtonOperator setPreview={setPreview} classes="buttons" value="clear" setResult={setResult}><span>AC</span></ClearButtonOperator>
-      <DeleteButtonOperator preview={preview} classes="buttons" value="delete" setPreview={setPreview}>
+      <DeleteButtonOperator preview={preview} classes="buttons" value="delete" setPreview={setPreview} finishedCalculating={finishedCalculating}>
         <span><i className="fa-solid fa-delete-left"></i></span>
       </DeleteButtonOperator>
-      <ButtonOperators onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} classes="buttons" value="%">
+      <ButtonOperators onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} preview={preview} classes="buttons" arithmetricValue="%">
         <span><i className="fa-solid fa-percent"></i></span>
       </ButtonOperators>
-      <ButtonOperators onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} classes="buttons" value="/">
+      <ButtonOperators onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} preview={preview} classes="buttons" arithmetricValue="/">
         <span><i className="fa-solid fa-divide"></i></span>
       </ButtonOperators>
       <ButtonNumber onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} classes="buttons" value="1"><span>1</span></ButtonNumber>
       <ButtonNumber onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} classes="buttons" value="2"><span>2</span></ButtonNumber>
       <ButtonNumber onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} classes="buttons" value="3"><span>3</span></ButtonNumber>
-      <ButtonOperators onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} classes="buttons" value="*">
+      <ButtonOperators onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} preview={preview} classes="buttons" arithmetricValue="*">
         <span><i className="fa-solid fa-xmark"></i></span>
       </ButtonOperators>
       <ButtonNumber onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} classes="buttons" value="4"><span>4</span></ButtonNumber>
       <ButtonNumber onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} classes="buttons" value="5"><span>5</span></ButtonNumber>
       <ButtonNumber onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} classes="buttons" value="6"><span>6</span></ButtonNumber>
-      <ButtonOperators onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} classes="buttons" value="-">
+      <ButtonOperators onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} preview={preview} classes="buttons" arithmetricValue="-">
         <span><i className="fa-solid fa-minus"></i></span>
       </ButtonOperators>
       <ButtonNumber onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} classes="buttons" value="7"><span>7</span></ButtonNumber>
       <ButtonNumber onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} classes="buttons" value="8"><span>8</span></ButtonNumber>
       <ButtonNumber onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} classes="buttons" value="9"><span>9</span></ButtonNumber>
-      <ButtonOperators onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} classes="buttons" value="+">
+      <ButtonOperators onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} preview={preview} classes="buttons" arithmetricValue="+">
         <span><i className="fa-solid fa-plus"></i></span>
       </ButtonOperators>
       <ButtonNumber onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} classes="buttons" value="0"><span>0</span></ButtonNumber>
-      <ButtonOperators onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} classes="buttons" value=".">
+      <ButtonOperators onFinishedCalculating={setFinishedCalculating} setPreview={setPreview} preview={preview} classes="buttons" arithmetricValue=".">
         <span><i className="fa-solid fa-period"></i></span>
       </ButtonOperators>
       <EqualSignButtonOperator
-        classes="buttons equals" value="=" setResult={setResult}
+        classes="buttons equals" setResult={setResult}
         preview={preview} setPreview={setPreview}
         onFinishedCalculating={setFinishedCalculating} finishedCalculating={finishedCalculating}>
         <span><i className="fa-solid fa-equals"></i></span>
@@ -56,16 +56,19 @@ export default function App() {
   </div>
 }
 
-function EqualSignButtonOperator({ classes, value, setResult, children, preview, setPreview, onFinishedCalculating, finishedCalculating }) {
-
+function EqualSignButtonOperator({ classes, setResult, children, preview, setPreview, onFinishedCalculating, finishedCalculating }) {
   function handleClick() {
-    if (finishedCalculating) return
-    let answer = evaluate(preview);
+    if (finishedCalculating) return;
+    let answer;
+    try {
+      answer = evaluate(preview);
+    } catch (error) {
+      alert("Invalid format used");
+      return;
+    }
     setResult(answer);
     onFinishedCalculating(true);
     setPreview(answer);
-    console.log("working")
-
   }
 
 
@@ -75,7 +78,7 @@ function EqualSignButtonOperator({ classes, value, setResult, children, preview,
 }
 
 
-function ClearButtonOperator({ classes, value, setPreview, children, setResult }) {
+function ClearButtonOperator({ classes, setPreview, children, setResult }) {
 
   function handleClick() {
     setPreview("");
@@ -89,9 +92,19 @@ function ClearButtonOperator({ classes, value, setPreview, children, setResult }
 }
 
 
-function DeleteButtonOperator({ children, classes, value, preview, setPreview }) {
+function DeleteButtonOperator({ children, classes, preview, setPreview, finishedCalculating }) {
+
   function handleTyping() {
-    setPreview(preview.slice(0, -1));
+    if (finishedCalculating) return;
+    let regex = /\s(?=\S$)/;
+    let hasSpaceBeforeLastCharacter = regex.test(preview);
+    let newPreview;
+    if (hasSpaceBeforeLastCharacter) {
+      newPreview = preview.slice(0, -2);
+    } else {
+      newPreview = preview.slice(0, -1);
+    }
+    setPreview(newPreview);
   }
 
   return (
@@ -100,16 +113,8 @@ function DeleteButtonOperator({ children, classes, value, preview, setPreview })
 }
 
 
-function ButtonOperators({ children, classes, value, setResult, setPreview, onFinishedCalculating }) {
-  function handleTyping() {
-    setPreview((p) => `${p} ${value} `);
-    onFinishedCalculating(false);
-  }
 
-  return (
-    <button className={classes} onClick={handleTyping}>{children}</button>
-  )
-}
+
 
 function ButtonNumber({ children, classes, value, setPreview, onFinishedCalculating }) {
   function handleTyping() {
@@ -121,6 +126,53 @@ function ButtonNumber({ children, classes, value, setPreview, onFinishedCalculat
     <button className={classes} onClick={handleTyping}>{children}</button>
   )
 }
+
+/* 
+function ButtonOperators({ children, classes, arithmetricValue, setPreview, onFinishedCalculating }) {
+  function handleTyping() {
+    setPreview(p => `${p} ${arithmetricValue} `)
+    onFinishedCalculating(false);
+  }
+
+
+  return (
+    <button className={classes} onClick={handleTyping}>{children}</button>
+  );
+} */
+
+
+function ButtonOperators({ children, classes, arithmetricValue, setPreview, onFinishedCalculating }) {
+  function handleTyping() {
+    setPreview((preview) => {
+      // Get the last character of the preview string
+      const lastChar = preview.slice(-1);
+
+      // Check if the last character is the same as the new value
+      if (lastChar === arithmetricValue) {
+        return preview; // If the value is already the last character, return the current preview
+      }
+
+      // Replace the last character with the new value or append the new value
+      let updatedPreview;
+      if (/\S/.test(lastChar)) { // Check if the last character is not a whitespace
+        updatedPreview = `${preview.slice(0, -1)} ${arithmetricValue}`;
+      } else {
+        updatedPreview = `${preview} ${arithmetricValue}`;
+      }
+
+      return updatedPreview;
+    });
+    
+    onFinishedCalculating(false);
+  }
+
+  return (
+    <button className={classes} onClick={handleTyping}>{children}</button>
+  );
+}
+
+
+
 
 function Preview({ preview }) {
   return (
